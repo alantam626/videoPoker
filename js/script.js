0f6960c35d5dace80ec89a0ed676056495d8f1ce
Alan Tam <alan@Alans-MacBook-Pro.local>
2021-11-22 14:23:31 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+
+/*----- constants -----*/
+// const cardDeck
+// const cardsInHand
+// const combos
+
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+
+// Build a 'master' deck of 'card' objects used to create shuffled decks
+
+/*----- app's state (variables) -----*/
+let shuffledDeck;
+let player = {
+    hand: [],
+    wallet: 0,
+    bet: 0
+}
+
+/*----- cached element references -----*/
+// const balance
+// player.hand
+// player.bet
+// const messageBox
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+
+function init() {
+
+}
+function deal() {
+
+}
+//reuse deal
+function render() {
+
+}
+function hold() {
+
+}
+function checkWin() {
+
+}
+function reset() {
+
+}
+
+
+
+// renderShuffledDeck();
