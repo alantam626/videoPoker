64775c0623a14a272d059534dae7abf93df623b3
Alan Tam <alan@Alans-MacBook-Pro.local>
2021-11-23 13:47:22 -0800

    Adds shuffling and card slot functionality to deal button

--- js/script.js
+++ js/script.js
@@ -1,52 +1,55 @@
 
 /*----- constants -----*/
-// const cardDeck
-// const cardsInHand
-// const combos
-
 const suits = ['s', 'c', 'd', 'h'];
 const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+const masterDeck = buildMainDeck()
 
-// Build a 'master' deck of 'card' objects used to create shuffled decks
+// renderInHTML(masterDeck, document.getElementById('mainCards'))
 
 /*----- app's state (variables) -----*/
 let shuffledDeck;
-let player = {
-    hand: [],
-    wallet: 0,
-    bet: 0
-}
 
 /*----- cached element references -----*/
-// const balance
-// player.hand
-// player.bet
-// const messageBox
-
+const shuffledMainCards = document.getElementById('shuffledMainCards')
 /*----- event listeners -----*/
-
+document.querySelector('button').addEventListener('click', renderShuffledDeck)
+// document.getElementById('cardslot0').addEventListener('click', holdCard)
 /*----- functions -----*/
+function renderShuffledDeck() {
+    const tempDeck = [...masterDeck];
+    shuffledDeck = []
+    
+    while (tempDeck.length) {
+        const rndIdx = Math.floor(Math.random() * tempDeck.length)
+        shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+    }
 
-function init() {
-
-}
-function deal() {
-
-}
-//reuse deal
-function render() {
-
-}
-function hold() {
-
-}
-function checkWin() {
-
-}
-function reset() {
-
+    const hand = shuffledDeck.slice(0,5)
+    renderInHTML(hand, shuffledMainCards)
 }
 
+function renderInHTML(hand, shuffledMainCards) {
+    shuffledMainCards.innerHTML = '';
+    const cardsHTML = hand.reduce(function(html, card) {
+        return html + `<div id="cardslot${hand.indexOf(card)}"class="card ${card.face}"></div>`;
+    }, '')
 
+    mainCards.innerHTML = cardsHTML
+}
 
-// renderShuffledDeck();
+function buildMainDeck() {
+    const mainDeck = [];
+    suits.forEach(function(suit) {
+        ranks.forEach(function(rank) {
+            mainDeck.push({
+                face: `${suit}${rank}`,
+                value: Number(rank) || (rank === 'A' ? 11 : 10)
+            })
+        })
+    })
+    return mainDeck;
+}
+
+// function holdCard() {
+    // hand.indexOf(card)
+// }
