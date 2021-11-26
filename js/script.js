8690f34043f41f3659315f1f6e2ab9d5511c5ed5
Alan Tam <alan@Alans-MacBook-Pro.local>
2021-11-26 00:00:48 -0800

    adds preliminary card hold function

--- js/script.js
+++ js/script.js
@@ -7,31 +7,34 @@ const masterDeck = buildMainDeck()
 // renderInHTML(masterDeck, document.getElementById('mainCards'))
 
 /*----- app's state (variables) -----*/
-let shuffledDeck;
+let shuffledDeck = []; 
+let hand;
+let heldCards = [];
 
 /*----- cached element references -----*/
 const shuffledMainCards = document.getElementById('shuffledMainCards')
+
 /*----- event listeners -----*/
-document.querySelector('button').addEventListener('click', renderShuffledDeck)
-// document.getElementById('cardslot0').addEventListener('click', holdCard)
+document.querySelector('button').addEventListener('click', shuffleDeck)
+document.getElementById('mainCards').addEventListener('click', holdCard)
+
 /*----- functions -----*/
-function renderShuffledDeck() {
+function shuffleDeck() {
     const tempDeck = [...masterDeck];
     shuffledDeck = []
-    
     while (tempDeck.length) {
         const rndIdx = Math.floor(Math.random() * tempDeck.length)
         shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
     }
 
-    const hand = shuffledDeck.slice(0,5)
+    hand = shuffledDeck.slice(0,5)
     renderInHTML(hand, shuffledMainCards)
 }
 
-function renderInHTML(hand, shuffledMainCards) {
+function renderInHTML(currentHand, shuffledMainCards) {
     shuffledMainCards.innerHTML = '';
-    const cardsHTML = hand.reduce(function(html, card) {
-        return html + `<div id="cardslot${hand.indexOf(card)}"class="card ${card.face}"></div>`;
+    const cardsHTML = currentHand.reduce(function(html, card) {
+        return html + `<div id="cardslot${currentHand.indexOf(card)}"class="card ${card.face}"></div>`;
     }, '')
 
     mainCards.innerHTML = cardsHTML
@@ -50,6 +53,20 @@ function buildMainDeck() {
     return mainDeck;
 }
 
-// function holdCard() {
-    // hand.indexOf(card)
-// }
+function holdCard(event) {
+    // grab the card class of the card we clicked
+    const cardClass = event.target.className.replace('card ', '')
+    // use the card class to find the card object in our hand
+    const heldCard = hand.find(card => card.face === cardClass)
+    const isCardHeld = heldCards.some(card => card.face === cardClass)
+    if(isCardHeld) {
+        // remove the card from the heldCards Array
+        // hints will need to use findIndex method to get idx
+        // use idx in .splice to actually remove
+        console.log('held, will be removed from heldCards array')
+    } else {
+        // add the card to the held cards array
+        // heldCards.push(heldCard)
+        console.log('push to heldCardss array')
+    }
+}   
